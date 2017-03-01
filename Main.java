3f87021e2bb77f4585ba64f46476ad4686a040d9
Dmytro Martyniuk <dmytro.martyniuk09@gmail.com>
2017-03-01 17:43:52 +0100

    Changed Main.java

--- Main.java
+++ Main.java
@@ -6,17 +6,6 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	   // int [] tab= {0,1,2,3,4};
-	    String str = "Hello";
-        char [] tab = str.toCharArray();
-        System.out.println(Arrays.toString(tab));
-        for(int i= tab.length/2; i >= 0; i--)
-        {
-            char tmp = tab[i];
-            tab[i] = tab[tab.length-1-i];
-            tab[tab.length-1-i] = tmp;
-        }
-        System.out.println(Arrays.toString(tab));
-        System.out.println(new String(tab));
+	   System.out.println("Hello, word!");
     }
 }
